2ff2ffd74f11936e27d5a2ad6b2d342e4f9283aa
Nishchal Ravish <ghanteravish@gmail.com>
2024-08-18 02:00:47 -0500

    Model Training File

--- train.py
+++ train.py
@@ -32,3 +32,78 @@ all_words = [stem(word) for word in all_words if word not in ignore_words]
 all_words = sorted(set(all_words))
 
 tags = sorted(set(tags))
+
+X_train = []
+y_train = []
+
+for(pattern_sentence, tag) in word_tag:
+    bag = bag_of_words(pattern_sentence,all_words)
+    X_train.append(bag)
+    label = tags.index(tag)
+    y_train.append(label)
+    
+X_train = np.array(X_train)
+y_train = np.array(y_train)
+
+num_epochs = 1000
+batch_size = 8
+learning_rate = 0.001
+input_size = len(X_train[0])
+hidden_size = 8
+output_size = len(tags)
+
+class ChatDataset(Dataset):
+    def __init__(self):
+        self.n_samples = len(X_train)
+        self.x_data = X_train
+        self.y_data = y_train
+        
+    def __getitem__(self,index):
+        return self.x_data[index], self.y_data[index]
+    
+    def __len__(self):
+        return self.n_samples
+    
+dataset = ChatDataset()
+
+train_loader = DataLoader(dataset=dataset,
+                          batch_size=batch_size,
+                          shuffle=True,
+                          num_workers=0)    
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+model = NeuralNet(input_size,hidden_size,output_size).to(device)
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+for epoch in range(num_epochs):
+    for (words, labels) in train_loader:
+        words = words.to(device)
+        labels = labels.to(dtype=torch.long).to(device)
+        
+        outputs = model(words)
+        
+        loss = loss_fn(outputs, labels)
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+    if (epoch+1) % 100 ==0:
+        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+
+model_dicts = {
+"model_state": model.state_dict(),
+"input_size": input_size,
+"hidden_size": hidden_size,
+"output_size": output_size,
+"all_words": all_words,
+"tags": tags
+}
+
+save_path = "model.pth"
+torch.save(model_dicts,save_path)
+
+print("Training done and file saved")
